64895cb4b1fdaf3fa0ec8d9eba1dc72de2a658a6
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2025-06-28 23:47:52 +0530

    hello api added

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 const formRoutes = require("./routes/formRoutes.js");
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hello APIs");
+});
+
 app.use("/api/form", formRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
